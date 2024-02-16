95699837d2d1c9665965efa35e2264c8e0f4a6b6
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-02-17 03:55:48 +0600

    fixed the banner section

--- src/ui/banner/Banner.jsx
+++ src/ui/banner/Banner.jsx
@@ -12,16 +12,19 @@ const Banner = () => {
                 Your browser does not support the video tag.
             </video>
             {/* You can add additional content over the video if needed */}
-            <div style={{ position: 'relative', zIndex: 1000, display: 'flex', justifyContent: "start", paddingTop: "120px", paddingLeft: "20px" }} className={Press.className}>
-                <h1 style={{ "font-size": "30px", "color": "white" }}>Explore, Compete, Conquer <br />Welcome to <span style={{ "color": 'red', "fontSize": "30px"}}>ZenGamesZone</span></h1>
+            <div style={{marginTop: "40px"}}>
+            <div style={{ position: 'relative', zIndex: 1000, display: 'flex',flexDirection:"column", justifyContent: "center", paddingTop: "120px", paddingLeft: "20px" }} className={Press.className}>
+                <h1 style={{ "font-size": "30px", "color": "white", width:"80%" , marginLeft:"auto", marginRight:"auto",paddingLeft:"50px"}}>Explore, Compete, Conquer Welcome to </h1>
+                <h1 style={{ "color": 'red', "fontSize": "30px", marginLeft:"auto", marginRight:"auto"}}>ZenGamesZone</h1>
             </div>
-            <div style={{ position: 'relative', zIndex: 1000, display: 'flex', justifyContent: "start", paddingTop: "15px", paddingLeft: "20px", width: "60%" }} className={Press.className}>
-                <h1 style={{ "font-size": "15px", "color": "white" }}><span style={{ "color": 'red' }}>ZenGamesZone</span>: A tranquil haven for gamers. Find flow in pixelated realms, seeking serenity and excitement in immersive experiences.</h1>
+            <div style={{ position: 'relative', zIndex: 1000, display: 'flex', justifyContent: "center", paddingTop: "15px", paddingLeft: "20px", width: "60%" , marginLeft: 'auto', marginRight: 'auto'}} className={Press.className}>
+                <h1 style={{ "font-size": "15px", "color": "white" , textAlign: "center"}}><span style={{ "color": 'red' }}>ZenGamesZone</span>: A tranquil haven for gamers. Find flow in pixelated realms, seeking serenity and excitement in immersive experiences.</h1>
             </div>
-            <div style={{paddingLeft: "20px", paddingTop: "20px"}}>
+            <div style={{paddingLeft: "20px", paddingTop: "20px", textAlign:"center"}}>
             <Button style={{paddingLeft: "60px", paddingRight: "60px", paddingTop: "13px" , paddingBottom: "13px", fontSize: "15px", backgroundColor: "red"}} variant="contained" color="error" endIcon={<FaBoxOpen />}>
             Join the Quest!
             </Button>
+            </div>
             </div>
         </div>
     );
